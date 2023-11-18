9ea3acb9f9ccb58d6c653cb5d42fa8077996f248
usamahussaindev <usamahussain.dev@gmail.com>
2023-11-18 22:48:08 +0500

    add : example includes Header, Footer, Body

---
+++ example/my-app/src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Intro1 from './components/Intro1';
+import Intro2 from './components/Intro2';
+import Intro3 from './components/Intro3';
+import Promo from './components/Promo';
+
+
+function App() {
+  return (
+    <div>
+      <Header />
+      {/* Other content goes here */}
+      <Intro1 />
+      <Promo />
+      <Intro2 />
+      <Intro3 />
+
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ example/my-app/src/components/Footer.js
@@ -0,0 +1,14 @@
+// Footer.js
+
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            {/* Your footer content goes here */}
+            <p>&copy; 2023 Your App Name</p>
+        </footer>
+    );
+}
+
+export default Footer;
