85a480647b3efe76026ba9ec26e4ff3719527539
usamahussaindev <usamahussain.dev@gmail.com>
2023-11-19 17:58:48 +0500

    props.children examples

--- Props-example/example2/src/App.js
+++ Props-example/example2/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Heading from './components/Heading';
-
+import Nav from './components/Nav';
 
 
 function App() {
@@ -8,7 +8,7 @@ function App() {
     <div>
       <Heading name="Software " color="Engineering" />
       {/* Other content goes here */}
-
+      <Nav first=" Usama" second=" Hussain" third=" Chaudary" />
     </div>
   );
 }
---
+++ Props-example/example2/src/components/Nav.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+const Nav = (props) => {
+    return (
+        <ul>
+            <li> {props.first}{props.second}{props.third} </li>
+        </ul>
+    )
+}
+
+export default Nav;
