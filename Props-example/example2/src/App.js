d9d501c039e4fc69f0fc0b9fbf04bbe237ced0cc
usamahussaindev <usamahussain.dev@gmail.com>
2023-11-21 23:42:07 +0500

    add number guessing game

--- Props-example/example2/src/App.js
+++ Props-example/example2/src/App.js
@@ -1,10 +1,11 @@
-import "./App.css";
-import Btn from "./components/Btn";
+
+
+import ModeToggler from "./components/ModeToggler";
 
 
 function App() {
     return (
-        <Btn />
+        <ModeToggler />
     );
 };
 
