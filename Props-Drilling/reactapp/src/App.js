8fe85d2909b6ef3c87e22ee6d5bf79c0fa658a8d
usamahussaindev <usamahussain.dev@gmail.com>
2023-11-24 03:58:35 +0500

    useReducer Hooks

--- Props-Drilling/reactapp/src/App.js
+++ Props-Drilling/reactapp/src/App.js
@@ -1,49 +1,13 @@
-import React from 'react';
-import Button from './Button';
 
-function Main(props) {
-  return <Header msg={props.msg} />;
-  //Note that when the Header component 
-  //is rendered from inside Main, it also receives the msg prop.
-};
-
-function Header(props) {
-  return (
-    <div style={{ border: "10px solid whitesmoke" }}>
-      <h1>Header here</h1>
-      <Wrapper msg={props.msg} />
-    </div>
-    //, the Header component’s function declaration has a return statement, 
-    //which renders the Wrapper component with the msg prop passed to it.
-  );
-};
-
-function Wrapper(props) {
-  return (
-    <div style={{ border: "10px solid lightgray" }}>
-      <h2>Wrapper here</h2>
-      <Button msg={props.msg} />
-    </div>
-    //rendering of the Button component, which also receives the msg attribute.
-  );
-};
-
-function Button(props) {
-  // the Button component’s function declaration is coded to receive the props object, 
-  return (
-    <div style={{ border: "20px solid orange" }}>
-      <h3>This is the Button component</h3>
-      <button onClick={() => alert(props.msg)}>Click me!</button>
-    </div>
-  );
-};
-
+import React from "react";
+import Counter from "./Components/Counter";
 function App() {
-  return (
-    <Main
-      msg="I passed through the Header and the Wrapper and I reached the Button component"
-    />
-  );
-};
+
+    return (
+        <div>
+            <Counter />
+        </div>
+    );
+}
 
 export default App;
---
+++ Props-Drilling/reactapp/src/Components/Counter.js
@@ -0,0 +1,28 @@
+import React, { useReducer } from 'react';
+
+// Reducer function - takes the current state and an action, returns the new state
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'INCREMENT':
+            return { count: state.count + 1 };
+        case 'DECREMENT':
+            return { count: state.count - 1 };
+        default:
+            return state;
+    }
+};
+
+const Counter = () => {
+    // useReducer returns the current state and a dispatch function
+    const [state, dispatch] = useReducer(reducer, { count: 0 });
+
+    return (
+        <div>
+            <p>Count: {state.count}</p>
+            <button onClick={() => dispatch({ type: 'INCREMENT' })}>Increment</button>
+            <button onClick={() => dispatch({ type: 'DECREMENT' })}>Decrement</button>
+        </div>
+    );
+};
+
+export default Counter;
