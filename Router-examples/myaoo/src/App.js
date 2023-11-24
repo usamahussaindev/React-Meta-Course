325ace9885ff2bec012e8666956412cfaf01619e
usamahussaindev <usamahussain.dev@gmail.com>
2023-11-25 01:00:34 +0500

    example ofrrouter

---
+++ Router-examples/myaoo/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'; // Corrected import statement for React
+import Contact from './Contact';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Contact />
+    </div>
+  );
+}
+
+export default App;
