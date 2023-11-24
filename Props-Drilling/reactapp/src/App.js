8835908fd43789d089ab7a603cd205c3903155f5
usamahussaindev <usamahussain.dev@gmail.com>
2023-11-25 00:56:57 +0500

    router example

--- Props-Drilling/reactapp/src/App.js
+++ Props-Drilling/reactapp/src/App.js
@@ -1,11 +1,19 @@
-
 import React from "react";
-import Counter from "./Components/Counter";
+import Fruits from "./Fruits";
+import FruitsCounter from "./FruitsCounter";
+
 function App() {
+    const [fruits] = React.useState([
+        { fruitName: 'apple', id: 1 },
+        { fruitName: 'apple', id: 2 },
+        { fruitName: 'plum', id: 3 },
+    ]);
 
     return (
-        <div>
-            <Counter />
+        <div className="App">
+            <h1>Where should the state go?</h1>
+            <Fruits fruits={fruits} />
+            <FruitsCounter fruits={fruits} />
         </div>
     );
 }
