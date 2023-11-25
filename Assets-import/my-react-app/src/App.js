1f31baf2bfbfef5b442af12af91c54ea0580d9eb
usamahussaindev <usamahussain.dev@gmail.com>
2023-11-25 06:06:19 +0500

    import sound assets

--- Assets-import/my-react-app/src/App.js
+++ Assets-import/my-react-app/src/App.js
@@ -1,23 +1,35 @@
-import Neno from './assets/neno.svg'; // Make sure to replace "your_image_filename.jpg" with the actual filename of your image
-
-
-import './App.css';
+import React from "react";
 
 function App() {
+
+  const bird1 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/9/9b/Hydroprogne_caspia_-_Caspian_Tern_XC432679.mp3"
+  );
+
+  const bird2 = new Audio(
+    "https://upload.wikimedia.org/wikipedia/commons/b/b5/Hydroprogne_caspia_-_Caspian_Tern_XC432881.mp3"
+  );
+
+  function toggle1() {
+    if (bird1.paused) {
+      bird1.play();
+    } else {
+      bird1.pause();
+    }
+  };
+
+  function toggle2() {
+    if (bird2.paused) {
+      bird2.play();
+    } else {
+      bird2.pause();
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Welcome to My React App!</h1>
-        <img src={Neno} alt="Neno" />
-        {/* Other components or content */}
-
-      </header>
-      <header2 className="App-header">
-        <h1>Welcome to My React App!</h1>
-        <img src={Neno} alt="Neno" />
-        {/* Other components or content */}
-
-      </header2>
+    <div>
+      <button onClick={toggle1}>Caspian Tern 1</button>
+      <button onClick={toggle2}>Caspian Tern 2</button>
     </div>
   );
 }
